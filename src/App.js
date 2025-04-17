bd9c13071cad742ec30578209847c33c770035db
Anjali jain <rohini1jb@gmail.com>
2025-04-17 20:16:14 +0530

    test commit2

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>hello....</h1>
-        <p>kushagra Jain bro of pratyaksh</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar/>
+        <Hero/>
+        <Search/>
       </header>
     </div>
   );
